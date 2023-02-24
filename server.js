28c161345d154663256a35929e43f9c00111228a
knotschii <sian.lazari@gmail.com>
2023-02-24 11:58:47 +0100

    auth routes implemented

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const db = require("./db.js")
 const cors = require("cors")
 const  shopinglistRouter = require("./routes/shopinglist.js")
 const  warehouseRouter = require ("./routes/warehouse.js")
+const authRoutes = require ("./routes/auth.js")
 
 const port = process.env.PORT || 5000;
 
@@ -14,7 +15,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
-app.use("/", shopinglistRouter, warehouseRouter)
+app.use("/", shopinglistRouter, warehouseRouter, authRoutes)
 
 app.listen(port, (req, res) => {
     console.log(`Server is running on ${port}`)
